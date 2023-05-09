96c9d6303c94fcec2269928238e979e7f3a4ea9d
Swastik  Deshmukh <91239444+swastik93-dot@users.noreply.github.com>
2023-05-09 23:23:38 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,17 @@
+//write a program which is use to perform addition of two numbers
+//ha static code ahea
+#include<stdio.h>
+
+int main()
+{
+  int a,b;
+  int c=0;
+
+  a=11;
+  b=20;
+
+  c=a+b;
+  printf("%d\n",c);  //& jr lihale tr address store hoil
+ 
+    return 0;
+}
